4489dcad1cfafe608b2f8ee6b2a16b3369f0af0b
Giovanni Bassi <giggio@giggio.net>
2022-09-22 20:16:44 -0300

    Add files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+Console.WriteLine(OldRegex.Bar("abc"));
+Console.WriteLine(NewRegex.Bar("abc"));
+
+public class OldRegex
+{
+    private static readonly Regex regex = new(@"abc|def", RegexOptions.IgnoreCase);
+    public static bool Bar(string input) => regex.IsMatch(input);
+}
+
+public partial class NewRegex
+{
+    [GeneratedRegex(@"abc|def", RegexOptions.IgnoreCase)]
+    private static partial Regex MyRegex();
+    public static bool Bar(string input) => MyRegex().IsMatch(input);
+}
